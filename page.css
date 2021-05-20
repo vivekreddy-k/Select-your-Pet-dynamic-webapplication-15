22f3f1484cfe3d583292ea30da65568db3fc3afd
vivek reddy Kasi Reddy <46521639+kasivivekkasivivek@users.noreply.github.com>
2021-05-20 22:12:00 +0530

    Create page.css

---
+++ page.css
@@ -0,0 +1,28 @@
+@import url("https://fonts.googleapis.com/css2?family=Bree+Serif&family=Caveat:wght@400;700&family=Lobster&family=Monoton&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Playfair+Display+SC:ital,wght@0,400;0,700;1,700&family=Playfair+Display:ital,wght@0,400;0,700;1,700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,700&family=Work+Sans:ital,wght@0,400;0,700;1,700&display=swap");
+
+.bg-container {
+    background-image: linear-gradient(to right, #d3d3d3, #e0e0e0);
+    border-radius: 36px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.form-control-class {
+    max-width: 400px;
+}
+
+.heading {
+    color: #2d3a35;
+    font-family: "Roboto";
+    font-size: 36px;
+    font-weight: 700;
+}
+
+.pet-image {
+    max-width: 400px;
+    max-height: 300px;
+
+}
